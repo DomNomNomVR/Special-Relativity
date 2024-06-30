dacbd7ca711ab497a9559927245cd922e1f004d8
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2024-06-30 13:01:16 +1200

    initial dump

---
+++ Assets/DomNomNom/Spacetime Globe/TrajectoryScaler.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.Animations;
+
+public class TrajectoryScaler : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+        Vector3 vel = GetComponent<LookAtConstraint>().GetSource(0).sourceTransform.localPosition;
+        Debug.Log(vel);
+        vel.x = 0;
+        float v = vel.magnitude;
+        float v_sq = v*v;
+
+        Vector3 localScale = transform.localScale;
+        localScale.z = Mathf.Sqrt((1 + v_sq) / (1 - v_sq));
+        transform.localScale = localScale;
+    }
+}
